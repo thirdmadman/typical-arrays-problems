a56bd03efbc1d50d1a3dabe56b09adec21580c1c
thirdmadman <thirdmadman@gmail.com>
2020-10-03 22:10:54 +0400

    impl: min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return ((array != null) && (array.length > 0)) ? (array.sort((a,b) => (a === b) ? 0 : (a < b ? -1 : 1))[0]) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return ((array != null) && (array.length > 0)) ? (array.sort((a,b) => (a === b) ? 0 : (a < b ? -1 : 1))[array.length-1]) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return ((array != null) && (array.length > 0)) ? (array.reduce((prev,cur,i) => prev + cur)/array.length) : 0;
 }
